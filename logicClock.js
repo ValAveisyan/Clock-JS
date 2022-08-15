37950f0f4f3e25c203bfb9c60f73cf50dfc1c460
valAvetisyan <val.avetisyan@outlook.com>
2022-08-15 11:54:09 +0400

    'Clock'

---
+++ logicClock.js
@@ -0,0 +1,31 @@
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let d = new Date();
+
+setInterval(
+    function () {
+
+
+        d = new Date();
+        seconds = d.getSeconds() * 6;
+        minutes = d.getMinutes() * 6 ;
+        hours = d.getHours() * 30;
+
+        const hour = document.getElementById('hours');
+        hour.style.transform = 'rotate(' + hours + 'deg)';
+
+
+        const second = document.getElementById('seconds');
+        second.style.transform = 'rotate(' + seconds + 'deg)';
+
+        const minute = document.getElementById('minutes');
+        minute.style.transform = 'rotate(' + minutes + 'deg)';
+
+
+    },1000
+);
+
+
